87812998ee409213e70b5d416b82916e3b3ba909
Brian Hogan <brianhogan@napcs.com>
2014-10-10 19:24:25 -0500

    fixes top stories url

--- plugins/hacker_news.rb
+++ plugins/hacker_news.rb
@@ -13,20 +13,20 @@ class HackerNews
 
   private
     def top_story(message)
-      message.reply get_story("first")
+      message.reply get_story(:first)
     end
   
     def random_story(message)
-      message.reply get_story("random") 
+      message.reply get_story(:random) 
     end
 
-    def get_story(rank)
-      url = "http://hacker-news.firebasio.com/v0/topstories.json"
+    def get_story(rank = :first)
+      url = "https://hacker-news.firebaseio.com/v0/topstories.json"
       begin
         raw_data = open(url).read
         story_ids = JSON.parse(raw_data)
         
-        rank == "first" ? id = story_ids.first : id = story_ids.shuffle.first
+        id = rank == :first ? story_ids.first : story_ids.shuffle.first
 
         url = "https://hacker-news.firebaseio.com/v0/item/#{id}.json"
 
