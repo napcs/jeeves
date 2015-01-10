25309cd1672d7ac410f0f08b406d91ae950c5313
arrio.farugie@gmail.com <arrio.farugie@gmail.com>
2015-01-10 17:56:16 -0600

    updated ip_lookup

--- plugins/ip_lookup.rb
+++ plugins/ip_lookup.rb
@@ -5,6 +5,8 @@ class IpLookup
 
   include Cinch::Plugin
 
+  $help_messages << "!ip_lookup <ip address> displays information about IP"
+  
   match /ip_lookup (.+)/, method: :getInfo
 
 
@@ -15,7 +17,7 @@ class IpLookup
       raw_data = open(url).read
       ipInfo = JSON.parse(raw_data)
 
-      output =  "#{ipInfo['longitude']} #{ipInfo['latitude']} #{ipInfo['ip']} #{ipInfo['area_code']} #{ipInfo['continent_code']}  #{ipInfo['timezone']} #{ipInfo['country_code']} #{ipInfo['isp']} #{ipInfo['country']} #{ipInfo['country_code3']}"
+      output =  "Longitude: #{ipInfo['longitude']} Latitude: #{ipInfo['latitude']} Ip: #{ipInfo['ip']} Area Code: #{ipInfo['area_code']} Continent: #{ipInfo['continent_code']} Timezone: #{ipInfo['timezone']} ISP: #{ipInfo['isp']} Country: #{ipInfo['country']} #{ipInfo['country_code3']}"
 
     rescue Exception => e
       output = "Something has gone wrong"
