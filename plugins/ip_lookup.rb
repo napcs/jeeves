efe9d6ec71b47eb97c76afeb4cc459e97a4a94fe
arrio.farugie@gmail.com <arrio.farugie@gmail.com>
2015-01-10 18:02:17 -0600

    updated ip_lookup

--- plugins/ip_lookup.rb
+++ plugins/ip_lookup.rb
@@ -17,7 +17,7 @@ class IpLookup
       raw_data = open(url).read
       ipInfo = JSON.parse(raw_data)
 
-      output =  "Longitude: #{ipInfo['longitude']} Latitude: #{ipInfo['latitude']} Ip: #{ipInfo['ip']} Area Code: #{ipInfo['area_code']} Continent: #{ipInfo['continent_code']} Timezone: #{ipInfo['timezone']} ISP: #{ipInfo['isp']} Country: #{ipInfo['country']} #{ipInfo['country_code3']}"
+      output =  "Longitude: #{ipInfo['longitude']}, Latitude: #{ipInfo['latitude']}, Ip: #{ipInfo['ip']}, Area Code: #{ipInfo['area_code']}, Continent: #{ipInfo['continent_code']}, Timezone: #{ipInfo['timezone']}, ISP: #{ipInfo['isp']}, Country: #{ipInfo['country']} #{ipInfo['country_code3']}"
 
     rescue Exception => e
       output = "Something has gone wrong"
