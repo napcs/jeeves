3fdaf5cd388614288d2b0ad6e706912886a242c3
arrio.farugie@gmail.com <arrio.farugie@gmail.com>
2015-01-10 22:27:33 -0600

    updated ip_lookup

--- plugins/ip_lookup.rb
+++ plugins/ip_lookup.rb
@@ -10,29 +10,27 @@ class IpLookup
   match /ip_lookup (.+)/
 
 
-  private 
-    def fetchInfo(ipAddress)
-      url = "http://www.telize.com/geoip/#{ipAddress}"
+  def fetchInfo(ipAddress)
+    url = "http://www.telize.com/geoip/#{ipAddress}"
 
-      begin
-        raw_data = open(url).read
-        ipInfo = JSON.parse(raw_data)
+    begin
+      raw_data = open(url).read
+      ipInfo = JSON.parse(raw_data)
 
-        output =  "Longitude: #{ipInfo['longitude']}, Latitude: #{ipInfo['latitude']}, Ip: #{ipInfo['ip']}, Area Code: #{ipInfo['area_code']}, Continent: #{ipInfo['continent_code']}, Timezone: #{ipInfo['timezone']}, ISP: #{ipInfo['isp']}, Country: #{ipInfo['country']} #{ipInfo['country_code4']}"
+      output =  "Longitude: #{ipInfo['longitude']}, Latitude: #{ipInfo['latitude']}, Ip: #{ipInfo['ip']}, Area Code: #{ipInfo['area_code']}, Continent: #{ipInfo['continent_code']}, Timezone: #{ipInfo['timezone']}, ISP: #{ipInfo['isp']}, Country: #{ipInfo['country']} #{ipInfo['country_code4']}"
 
-      rescue SocketError => e 
-        output = "Socket error"
-      rescue OpenURI::HTTPError => e
-        output = "Argument Error"
-      rescue ArgumentError => e
-        output = "default argument error"
-      end
-
-      output
+    rescue SocketError => e 
+      output = "Socket error"
+    rescue OpenURI::HTTPError => e
+      output = "Argument Error"
     end
 
-  public
-    def execute(m, ipAddress)
-      m.reply(fetchInfo(ipAddress))
-    end
+    output
+  end
+
+  def execute(m, ipAddress)
+    m.reply(fetchInfo(ipAddress))
+  end
+
+    private :fetchInfo
 end
