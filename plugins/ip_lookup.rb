2285683ddcafa31d28c6b722aac95c3653a703d7
arrio.farugie@gmail.com <arrio.farugie@gmail.com>
2015-01-10 21:46:21 -0600

    updated ip_lookup

--- plugins/ip_lookup.rb
+++ plugins/ip_lookup.rb
@@ -5,25 +5,34 @@ class IpLookup
 
   include Cinch::Plugin
 
-  $help_messages << "!ip_lookup <ip address> displays information about IP"
+  $help_messages << "!ip_lookup <ip address>: displays information about IP"
   
-  match /ip_lookup (.+)/, method: :getInfo
+  match /ip_lookup (.+)/
 
 
-  def getInfo(m, ipAddress)
-    url = "http://www.telize.com/geoip/#{ipAddress}"
+  private 
+    def fetchInfo(ipAddress)
+      url = "http://www.telize.com/geoip/#{ipAddress}"
 
-    begin
-      raw_data = open(url).read
-      ipInfo = JSON.parse(raw_data)
+      begin
+        raw_data = open(url).read
+        ipInfo = JSON.parse(raw_data)
 
-      output =  "Longitude: #{ipInfo['longitude']}, Latitude: #{ipInfo['latitude']}, Ip: #{ipInfo['ip']}, Area Code: #{ipInfo['area_code']}, Continent: #{ipInfo['continent_code']}, Timezone: #{ipInfo['timezone']}, ISP: #{ipInfo['isp']}, Country: #{ipInfo['country']} #{ipInfo['country_code3']}"
+        output =  "Longitude: #{ipInfo['longitude']}, Latitude: #{ipInfo['latitude']}, Ip: #{ipInfo['ip']}, Area Code: #{ipInfo['area_code']}, Continent: #{ipInfo['continent_code']}, Timezone: #{ipInfo['timezone']}, ISP: #{ipInfo['isp']}, Country: #{ipInfo['country']} #{ipInfo['country_code4']}"
 
-    rescue Exception => e
-      output = "Something has gone wrong"
+      rescue SocketError => e 
+        output = "Socket error"
+      rescue OpenURI::HTTPError => e
+        output = "Argument Error"
+      rescue ArgumentError => e
+        output = "default argument error"
+      end
+
+      output
     end
 
-    m.reply output
-
-  end
+  public
+    def execute(m, ipAddress)
+      m.reply(fetchInfo(ipAddress))
+    end
 end
