7c62b60254f85d61f25c6e46760ad3d27eb4d977
Brian Hogan <brianhogan@napcs.com>
2015-01-15 08:40:04 -0600

    update info plugin to support no query

--- plugins/info.rb
+++ plugins/info.rb
@@ -11,20 +11,15 @@
 # Use !info help   if you want to see all the keys.
 class Info
 
-  # include the Cinch::Plugin mixin which 
-  # adds the behavior for plugins to this plain Ruby object.
   include Cinch::Plugin
 
-  # Append a new message to the array of messages.
   $help_messages << "!info <key> : displays some static content. Try typing just !info"
   match /info (.+)/
 
   def execute(message, query=nil)
-    
+
     if query
-      # message.reply sends a message to the channel.
-      # query is the captured value from the regular expression
-      # in the matcher.
+
       if data[query]
         message.reply(data[query])
       else
@@ -32,19 +27,17 @@ class Info
           refresh_data
           message.reply("Data refreshed.")
         elsif query == "help"
-          message.reply("Choose from #{data.keys.join(", ")}")
+          display_help
         else
           message.reply("I don't know that")
         end
       end
+    else
+      display_help
     end
-   
-    # and that's it. The rest is up to you! Use any Ruby code
-    # you want, including other objects, modules, Gems, whatever!
-    # Make an awesome thing!
 
   end
-  
+
   def data
     $data ||= read_from_file
   end
@@ -56,9 +49,12 @@ class Info
   private
     def read_from_file
       begin
-        YAML.load(File.read("info.yml")) 
+        YAML.load(File.read("info.yml"))
       end
     end
 
+    def display_help
+      message.reply("Choose from #{data.keys.join(", ")}")
+    end
 
 end
