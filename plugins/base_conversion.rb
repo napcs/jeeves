3f80494ff09f7414bd0779b1a361bc030e14dcf9
arrio.farugie@gmail.com <arrio.farugie@gmail.com>
2015-02-02 02:40:55 -0600

    wrap everything in a class, create String and Binary objects & methods

--- plugins/base_conversion.rb
+++ plugins/base_conversion.rb
@@ -1,84 +1,82 @@
-#Author: Arrio Farugie
-#
-#BaseConversion: Converts strings into binary or hexadecimal!
-#
-#Special thanks to: amitchelbullard
-
-class BaseConversion
-
-
-  include Cinch::Plugin
-
-
-  $help_messages << "!to_binary:  converts string to binary"
-  $help_messages << "!to_hex:  converts string to hexadecimal"
-
-
-  match /to_binary (.+)/, method: :displayBinary
-  match /to_hex (.+)/, method: :displayHex
-
-
-  #fixes rubys concatenated binary bytes, eg. 1 = 1, 2 = 10
-  def setLength(binary)
-    while binary.length % 8 != 0
-      binary = '0' + binary
-    end
-    binary
+class String
+  attr_accessor :string
+  def initialize(string)
+    @string = string
   end
-
-
-  #create extra spaces around space byte so user can easily see them
-  def addSpace(value)
-    value = '  ' + value + '  '
-  end
-
-  def convertToBinary(message)
+ 
+  def convert(base)
     output = Array.new
-
-    message.each_byte do |c|
-      binary = c.to_s(2)
-
-      if binary.length % 8 != 0
-        binary = setLength(binary)
-      end
-
-      #00100000 = space byte
-      if binary == '00100000'
-        binary = addSpace(binary)
-      end
-
+    @string.each_byte do|c|
+      binary = c.to_s(base)
       output.push binary
     end
-
     output.join(' ')
   end
 
+  def toBinary
+    convert(2)
+  end
+
+  def toHex
+    convert(16)
+  end
+end
 
 
+class Binary
+  attr_accessor :value
+  def initialize(value)
+    @value = value
+  end
 
-  def convertToHex(message)
+  def toInt
     output = Array.new
-
-    message.each_byte do |c|
-      hex = c.to_s(16)
-
-      #20 = space byte
-      if hex == '20'
-        hex = addSpace(hex)
-      end
-
-      output.push hex
+    newValue = @value.split(' ')
+    newValue.each do |a|
+      output.push a.to_i(2)
     end
-     output.join(' ')
+    output.join(' ')
   end
 
+  def toString
+    output = Array.new
+    newBinary = @value.scan(/.{8}|.+/).join(' ').split(' ')
+    newBinary.each do |a|
+      output.push a.to_i(2).chr
+    end
+    output.join
+  end
+end
 
-  def displayBinary(m, message)
-    m.reply convertToBinary(message)
+
+class BaseConversion
+  include Cinch::Plugin
+
+  $help_messages << "!to_binary   :converts string to binary"
+  $help_messages << "!to_hex :converts string to hex"
+  $help_messages << "!binary_to_integer :converts binary to integer"
+  $help_messages << "!binary_to_string  :converts binary to string"
+
+  match /to_binary (.+)/, method: :toBinary
+  match /to_hex (.+)/, method: :toHex
+  match /binary_to_integer (.+)/, method: :binaryToInteger
+  match /binary_to_string (.+)/, method: :binaryToString
+
+  def toBinary(m, message)
+    m.reply String.new(message).toBinary
   end
 
-  def displayHex(m, message)
-    m.reply convertToHex(message)
+  def toHex(m, message)
+    m.reply String.new(message).toHex
+  end
+
+  def binaryToInteger(m, message)
+    m.reply Binary.new(message).toInt
+  end
+
+  def binaryToString(m, message)
+    m.reply Binary.new(message).toString
   end
 
 end
+
