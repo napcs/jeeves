17523b3b8a7be49df0ee8f4ce9d2f5aaead8fd40
arrio.farugie@gmail.com <arrio.farugie@gmail.com>
2015-02-02 19:24:47 -0600

    change name of classes to better reflect what each class does

--- plugins/base_conversion.rb
+++ plugins/base_conversion.rb
@@ -1,40 +1,48 @@
-class String
-  attr_accessor :string
-  def initialize(string)
-    @string = string
+class StringConverter
+  attr_accessor :string_value
+
+  def initialize(string_value)
+    @string_value = string_value
   end
+
   def convert(base)
     output = Array.new
-    @string.each_byte do|c|
+    @string_value.each_byte do|c|
       binary = c.to_s(base)
       output.push binary
     end
     output.join(' ')
   end
-  def toBinary
+
+  def to_binary
     convert(2)
   end
-  def toHex
+
+  def to_hex
     convert(16)
   end
 end
 
-class Binary
-  attr_accessor :value
-  def initialize(value)
-    @value = value
+
+class BinaryConverter
+  attr_accessor :binary_value
+
+  def initialize(binary_value)
+    @binary_value = binary_value
   end
-  def toInt
+
+  def to_int
     output = Array.new
-    tempValue = @value.split(' ')
+    tempValue = @binary_value.split(' ')
     tempValue.each do |a|
       output.push a.to_i(2)
     end
     output.join(' ')
   end
-  def toString
+
+  def to_string
     output = Array.new
-    tempValue = @value.scan(/.{8}|.+/).join(' ').split(' ')
+    tempValue = @binary_value.delete(' ').scan(/.{8}|.+/).join(' ').split(' ')
     tempValue.each do |a|
       output.push a.to_i(2).chr
     end
@@ -44,27 +52,31 @@ end
 
 class BaseConversion
   include Cinch::Plugin
+
   $help_messages << "!to_binary   :converts string to binary"
   $help_messages << "!to_hex :converts string to hex"
   $help_messages << "!binary_to_integer :converts binary to integer"
   $help_messages << "!binary_to_string  :converts binary to string"
 
-  match /to_binary (.+)/, method: :toBinary
-  match /to_hex (.+)/, method: :toHex
-  match /binary_to_integer (.+)/, method: :binaryToInteger
-  match /binary_to_string (.+)/, method: :binaryToString
+  match /to_binary (.+)/, method: :string_to_binary
+  match /to_hex (.+)/, method: :string_to_hex
+  match /binary_to_integer (.+)/, method: :binary_to_integer
+  match /binary_to_string (.+)/, method: :binary_to_string
 
-  def toBinary(m, message)
-    m.reply String.new(message).toBinary
+  def string_to_binary(m, message)
+    m.reply StringConverter.new(message).to_binary
   end
-  def toHex(m, message)
-    m.reply String.new(message).toHex
+
+  def string_to_hex(m, message)
+    m.reply StringConverter.new(message).to_hex
   end
-  def binaryToInteger(m, message)
-    m.reply Binary.new(message).toInt
+
+  def binary_to_integer(m, message)
+    m.reply BinaryConverter.new(message).to_int
   end
-  def binaryToString(m, message)
-    m.reply Binary.new(message).toString
+
+  def binary_to_string(m, message)
+    m.reply BinaryConverter.new(message).to_string
   end
-end
+ end
 
