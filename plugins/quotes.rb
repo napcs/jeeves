7f538a47483934f61dd7c552f121bfe81cca0713
TopShelfBullard <amb@amitchellbullard.com>
2015-02-09 01:41:38 -0600

    Create a config directory, move quotes.txt into into the config/quotes/ directory, and handle the eventuality of the plugin being unable to find the quotes.txt file by alerting the author of the plugin, the owner of the channel, and then printing a default quote. Note: The author, owner, and default quote are all hard coded. These should be in a config file with a separate class that loads the configuration or they should possibly be environment variables. Also, having a environment variable that sets the quotes.txt file differently for development, production, and test  would make the unit tests less crazy

--- plugins/quotes.rb
+++ plugins/quotes.rb
@@ -1,15 +1,62 @@
 class Quotes
   include Cinch::Plugin
-  require_relative 'quotes/lib/quote_source'
-  require_relative 'quotes/lib/quote_display'
 
-  $help_messages << '!quote  :  A random quote will be printed to the screen.'
-  $help_messages << '!quote <quote> : Add a quote to the list of quotes to be displayed. Quote must be properly attributed and must be at least 27 characters long. ex: The dude abides. - The Dude'
+  $help_messages << "!quote  :  A random quote will be printed to the screen."
+  $help_messages << "!quote <quote> : Add a quote to the list of quotes to be displayed. Quote must be properly attributed and must be at least 27 characters long. ex: The dude abides. - The Dude"
 
   match /quote(.*)/, method: :run_quotes
 
   def run_quotes(m, message = nil)
+    puts "add_quote (m, #{message})"
     quote_source = QuoteSource.new(message)
+    puts "quote_source.quote = #{quote_source.quote})"
     QuoteDisplay.output(m, quote_source.quote)
+  end
+end
+
+class QuoteSource
+  attr_reader :quote
+
+  ACCEPTABLE_QUOTE_LENGTH = 25
+  QUOTE_ATTRIBUTION_FORMAT = " - "
+  CHANNEL_OWNER_NICK = ":owner"
+  PLUGIN_AUTHOR_NICK = ":amitchellbullard"
+  QUOTES_FILE = "config/quotes/quotes.txt"
+  DEFAULT_QUOTE = "Whether you are designing systems or individual modules, never forget to use the simplest thing that can possibly work. - Robert C. Martin"
+  QUOTE_FILE_NOT_FOUND_MESSAGE = "Attention #{PLUGIN_AUTHOR_NICK} and #{CHANNEL_OWNER_NICK}: Unable to find the quotes file. Displaying default quote..."
+  DEFAULT_MESSAGE = "#{QUOTE_FILE_NOT_FOUND_MESSAGE}\n#{DEFAULT_QUOTE}"
+
+  def initialize(quote)
+    quote = regularize_whitespace(quote)
+    @quote = quote_is_invalid(quote) ? random_quote : add_quote(quote)
+  end
+
+  private
+
+  def random_quote
+    File.exists?(QUOTES_FILE) ? IO.readlines(QUOTES_FILE).shuffle.first : DEFAULT_MESSAGE
+  end
+
+  def add_quote(quote)
+    file = File.exists?(QUOTES_FILE) ? File.open(QUOTES_FILE, "a") {|file| file.puts quote} : DEFAULT_MESSAGE
+  end
+
+  def regularize_whitespace(quote)
+    quote.strip.squeeze(" ") unless quote.nil?
+  end
+
+  def quote_is_invalid(quote)
+    quote.nil? || quote.empty? || quote.length < ACCEPTABLE_QUOTE_LENGTH || !quote.include?(QUOTE_ATTRIBUTION_FORMAT)
+  end
+end
+
+class QuoteDisplay
+  class << self
+
+    def output(m, quote)
+      output = quote.nil? || quote.empty? ? "Quote added." : quote
+      m.reply output
+    end
+
   end
 end
--- test/unit/quotes_test.rb
+++ test/unit/quotes_test.rb
@@ -1,43 +1,83 @@
-require 'test_helper.rb'
-require 'quotes'
+require "test_helper.rb"
+require "quotes"
 
 class QuotesTest < MiniTest::Test
   def move_test_quotes_in
-    FileUtils.mv('plugins/quotes/resources/quotes.txt', 'plugins/quotes/resources/tmp/quotes.txt')
-    FileUtils.mv('plugins/quotes/resources/quotes_test.txt', 'plugins/quotes/resources/quotes.txt')
+    FileUtils.mv("config/quotes/quotes.txt", "config/quotes/test_resources/quotes.txt")
+    FileUtils.mv("config/quotes/test_resources/test_quotes.txt", "config/quotes/quotes.txt")
   end
 
   def move_test_quotes_out
-    FileUtils.mv('plugins/quotes/resources/quotes.txt', 'plugins/quotes/resources/quotes_test.txt')
-    FileUtils.mv('plugins/quotes/resources/tmp/quotes.txt', 'plugins/quotes/resources/quotes.txt')
+    FileUtils.mv("config/quotes/quotes.txt", "config/quotes/test_resources/test_quotes.txt")
+    FileUtils.mv("config/quotes/test_resources/quotes.txt", "config/quotes/quotes.txt")
+  end
+
+  def quote_file_does_not_exists
+    FileUtils.mv("config/quotes/quotes.txt", "config/quotes/test_resources/quotes.txt")
+  end
+
+  def quote_file_exists
+    FileUtils.mv("config/quotes/test_resources/quotes.txt", "config/quotes/quotes.txt")
   end
 
   def test_quote_is_returned
     move_test_quotes_in
-
     bot = make_bot(Quotes)
-    message = make_message(bot, '!quote')
+    message = make_message(bot, "!quote")
     quote = get_replies(message).first
     quote = quote.strip.squeeze(' ') unless quote.nil?
-
     move_test_quotes_out
 
-    assert !quote.nil? && !quote.empty? && quote.length >= 'The Dude abides - Jeffery Lebowski.'.length && quote.include?(' - ')
+    assert !quote.nil? && !quote.empty? && quote.length > 24 && quote.include?(' - ')
   end
 
   def test_quote_addition_message_is_displayed
     move_test_quotes_in
     bot = make_bot(Quotes)
-    message = make_message(bot, '!quote The Dude abides - Jeffery Lebowski.')
+    message = make_message(bot, "!quote The Dude abides - Jeffery Lebowski.")
     replies = get_replies(message)
     move_test_quotes_out
 
-    assert_equal 'Quote added.', replies.first
+    assert_equal "Quote added.", replies.first
+  end
+
+  def test_default_message_is_displayed_when_unable_to_read_quotes
+    plugin_author_nick = ":amitchellbullard"
+    channel_owner_nick = ":bphogan"
+    default_quote = "Whether you are designing systems or individual modules, never forget to use the simplest thing that can possibly work. - Robert C. Martin"
+    quote_file_not_found_message = "Attention #{plugin_author_nick} and #{channel_owner_nick}: Unable to find the quotes file. Displaying default quote..."
+    default_message = "#{quote_file_not_found_message}\n#{default_quote}"
+
+    quote_file_does_not_exists
+    bot = make_bot(Quotes)
+    message = make_message(bot, "!quote")
+    default = get_replies(message).first
+    default = default.strip.squeeze(' ') unless default.nil?
+    quote_file_exists
+
+    assert_equal default, default_message
+  end
+
+  def test_default_message_is_displayed_when_unable_to_write_quotes
+    plugin_author_nick = ":amitchellbullard"
+    channel_owner_nick = ":bphogan"
+    default_quote = "Whether you are designing systems or individual modules, never forget to use the simplest thing that can possibly work. - Robert C. Martin"
+    quote_file_not_found_message = "Attention #{plugin_author_nick} and #{channel_owner_nick}: Unable to find the quotes file. Displaying default quote..."
+    default_message = "#{quote_file_not_found_message}\n#{default_quote}"
+
+    quote_file_does_not_exists
+    bot = make_bot(Quotes)
+    message = make_message(bot, "!quote The Dude abides - Jeffery Lebowski.")
+    default = get_replies(message).first
+    default = default.strip.squeeze(' ') unless default.nil?
+    quote_file_exists
+
+    assert_equal default, default_message
   end
 
   def test_all_quotes_are_valid
-    IO.readlines('plugins/quotes/resources/quotes.txt').each do |quote|
-      assert !quote.nil? && !quote.empty? && quote.length >= 'The Dude abides - Jeffery Lebowski.'.length && quote.include?(' - ')
+    IO.readlines("config/quotes/quotes.txt").each do |quote|
+      assert !quote.nil? && !quote.empty? && quote.length > 24 && quote.include?(' - ')
     end
   end
 
