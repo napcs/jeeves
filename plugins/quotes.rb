3291479dc69077619b66325614a7e9dc6f14a8a6
TopShelfBullard <amb@amitchellbullard.com>
2015-02-10 21:29:36 -0600

    Refactor the QuoteSource class back into the main quotes plugin class.

--- plugins/quotes.rb
+++ plugins/quotes.rb
@@ -4,50 +4,40 @@ class Quotes
   $help_messages << "!quote  :  A random quote will be printed to the screen."
   $help_messages << "!quote <quote> : Add a quote to the list of quotes to be displayed. Quote must be properly attributed and must be at least 27 characters long. ex: The dude abides. - The Dude"
 
-  match /quote(.*)/, method: :run_quotes
+  match /quote(.*)/, method: :run_quotes_plugin
 
-  def run_quotes(m, message = nil)
-    quote_source = QuoteSource.new(message)
-    output(m, quote_source.quote)
-  end
-
-  private
-
-  def output(m, quote)
-    output = quote.nil? || quote.empty? ? "Quote added." : quote
-    m.reply output
-  end
-end
-
-class QuoteSource
-  attr_reader :quote
-
-  QUOTES_FILE = "config/quotes/quotes.txt"
+  QUOTES_FILE_PATH = "config/quotes/quotes.txt"
   QUOTE_FILE_ERROR = "amitchellbullard: Unable to load the quotes file."
   ACCEPTABLE_QUOTE_LENGTH = 25
   QUOTE_ATTRIBUTION_FORMAT = " - "
 
-  def initialize(quote)
-    quote = regularize_whitespace(quote)
-    @quote = quote_is_invalid(quote) ? random_quote : add_quote(quote)
+  def run_quotes_plugin(m, message = nil)
+    return QUOTE_FILE_ERROR unless File.exists?(QUOTES_FILE_PATH)
+    message = regularize_whitespace(message)
+    output = is_invalid_quote(message) ? get_random_quote : add_quote(message)
+    output(m, output)
   end
 
   private
 
-  def random_quote
-    return QUOTE_FILE_ERROR unless File.exists?(QUOTES_FILE)
-    IO.readlines(QUOTES_FILE).shuffle.first
+  def get_random_quote
+    IO.readlines(QUOTES_FILE_PATH).shuffle.first
   end
 
   def add_quote(quote)
-    File.open(QUOTES_FILE, "a") {|file| file.puts quote}
+    File.open(QUOTES_FILE_PATH, "a") {|file| file.puts quote}
+    "Quote added."
+  end
+
+  def is_invalid_quote(quote)
+    quote.nil? || quote.empty? || quote.length < ACCEPTABLE_QUOTE_LENGTH || !quote.include?(QUOTE_ATTRIBUTION_FORMAT)
   end
 
   def regularize_whitespace(quote)
     quote.strip.squeeze(" ") unless quote.nil?
   end
 
-  def quote_is_invalid(quote)
-    quote.nil? || quote.empty? || quote.length < ACCEPTABLE_QUOTE_LENGTH || !quote.include?(QUOTE_ATTRIBUTION_FORMAT)
+  def output(m, output)
+    m.reply output
   end
 end
