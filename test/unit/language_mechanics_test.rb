67b39106eb56cd29962ea80ed116d8288adf46b5
Alexander Henry <alexanderhenry21@gmail.com>
2015-09-08 15:10:39 -0500

    Changed file names from grammar nazi to language mechanics.  Added grammar to correct people on, using more courteous language. Began working on a hash/array to integrate language mechanics to use meme generator.

---
+++ test/unit/language_mechanics_test.rb
@@ -0,0 +1,302 @@
+# Include the test setup:
+require 'test_helper.rb'
+
+# include the plugin you are testing
+require 'language_mechanics'
+
+# Define the test class
+class LanguageMechanicsTest < MiniTest::Test
+
+  # Define your first test case.
+  
+  #def test_comma_outside_of_quotation
+  #  bot = make_bot(LanguageMechanics)
+  #  message = make_message(bot, '",')
+  #  replies = get_replies(message)
+  #  assert_equal "It is correct to keep the punctuation inside of the quotations, not outside of them.", replies.first
+  #end
+
+  #def test_exclamation_point_outside_of_quotation
+  #  bot = make_bot(LanguageMechanics)
+  #  message = make_message(bot, '"!')
+  #  replies = get_replies(message)
+  #  assert_equal "It is correct to keep the punctuation inside of the quotations, not outside of them.", replies.first
+  #end
+
+  #def test_period_outside_of_quotation
+  #  bot = make_bot(LanguageMechanics)
+  #  message = make_message(bot, '".')
+  #  replies = get_replies(message)
+  #  assert_equal "It is correct to keep the punctuation inside of the quotations, not outside of them.", replies.first
+  #end
+
+  #def test_question_mark_outside_of_quotation
+  #  bot = make_bot(LanguageMechanics)
+  #  message = make_message(bot, '"?')
+  #  replies = get_replies(message)
+  #  assert_equal "It is correct to keep the punctuation inside of the quotations, not outside of them.", replies.first
+  #end
+
+  #def test_alot
+
+  #  bot = make_bot(LanguageMechanics)
+
+  #  message = make_message(bot, 'alot')
+
+  #  replies = get_replies(message)
+
+  #  assert_equal "To refer to many of something please use \"a lot.\"  To refer to setting aside money please use \"allot.\"", replies first
+
+  #end
+
+  def test_ass
+
+    bot = make_bot(LanguageMechanics)
+    message = make_message(bot, 'ass')
+    replies = get_replies(message)
+    assert_equal "Unless you're referring to a donkey, please use a more courteous word. Options include \"arse\" and \"bum.\"", replies.first
+  end
+
+  def test_coulda
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'coulda')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "Please use \"could have\" or \"could've.\"", replies.first
+
+  end
+
+  def test_couldve
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'couldve')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "Don't forget your apostrophe!", replies.first
+
+  end
+
+  def test_fuck
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'fuck')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "I think you meant \"fudge.\"", replies.first
+
+  end
+
+  def test_if_i_was
+
+    bot = make_bot(LanguageMechanics)
+    message = make_message(bot, 'if i was')
+    replies = get_replies(message)
+    assert_equal "Please use the subjunctive.  The correct way to say this is \"if I were.\"", replies.first
+
+  end
+
+  def test_l8r
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'l8r')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "Please type as if you were writing a professional email to a colleague.", replies.first
+
+  end
+
+  def test_peaked_my
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'peaked my')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "The word is \"piqued.\"", replies.first
+
+  end
+
+  def test_peeked_my
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'peeked my')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "The word is \"piqued.\"", replies.first
+
+  end
+
+  def test_shit
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'shit')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "I think you meant \"shiitake.\"", replies.first
+
+  end
+
+  def test_shoulda
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'You shoulda come over')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "Please use \"should have\" or \"should've.\"", replies.first
+
+  end
+
+  def test_shouldve
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'You shouldve come over')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "Don't forget your apostrophe!", replies.first
+
+  end
+
+  def test_sneak_peak
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'Sneak peak')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "Sneak peek, not sneak peak. Please visit https://twitter.com/stealthmountain for more information on this.", replies.first
+
+  end
+
+  def test_sneek_peak
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'Sneek peak')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "Sneak peek, not sneek peak. Please visit https://twitter.com/stealthmountain for more information on this.", replies.first
+
+  end
+
+  def test_sneek_peek
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'Sneek peek')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    assert_equal "Sneak peek, not sneek peek. Please visit https://twitter.com/stealthmountain for more information on this.", replies.first
+
+  end
+
+  def test_whos_is
+    bot=make_bot(LanguageMechanics)
+    message=make_message(bot, 'who\'s is')
+    replies=get_replies(message)
+    assert_equal "\"Who\'s is\" refers to \"Who is.\"  You'll want to use \"whose.\"", replies.first
+  end
+
+  def test_wtf
+
+    # Create a bot. Pass the class name of your plugin
+    bot = make_bot(LanguageMechanics)
+
+    # Send the bot a message
+    message = make_message(bot, 'wtf')
+
+    # Get the response
+    replies = get_replies(message)
+
+    # now do your assertion
+    # Your username is 'test'
+    #
+    # THIS IS FAILING BECAUSE RAKE TEST IS ALSO RUNNING THE MEME GENERATOR TEST, WHICH IS NOT WORKING
+    assert_equal "What does this even mean? Please type it out in full.", replies.first
+    #assert_equal "http://apimeme.com/meme?meme=One+Does+Not+Simply&top=One+Does+Not+Simply&bottom=Abbreviate+Anger", replies.first
+    #assert_equal "* grammar_snob would like you to review this website https://www.grammarly.com", replies.first
+
+  end
+
+
+end
