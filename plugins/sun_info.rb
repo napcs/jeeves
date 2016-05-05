5b853cd97c83d0042e26a355fc807af3accd26e2
AlexanderHenry <alexanderhenry21@gmail.com>
2016-05-04 22:55:45 -0500

    Fixed spacing issues

--- plugins/sun_info.rb
+++ plugins/sun_info.rb
@@ -3,26 +3,26 @@ require 'nokogiri'
 require 'cgi'
 
 class SunInfo
-	$help_messages << "!sun_info <country>, <city>	: Display current information about the sun"
+  $help_messages << "!sun_info <country>, <city>	: Display current information about the sun"
 	
-	include Cinch::Plugin
-	match /sun_info (.*),\s?(.*)/
+  include Cinch::Plugin
+  match /sun_info (.*),\s?(.*)/
 
-	def execute(m, country, city)
-	url = "http://www.timeanddate.com/sun/#{country.downcase.gsub(" ", "-")}/#{city.downcase.gsub(" ", "-")}"
-	main_div = Nokogiri::HTML(open(url)).at("div#qlook")
+  def execute(m, country, city)
+    url = "http://www.timeanddate.com/sun/#{country.downcase.gsub(" ", "-")}/#{city.downcase.gsub(" ", "-")}"
+    main_div = Nokogiri::HTML(open(url)).at("div#qlook")
   	
-  #Future proofing for grabbing the distance
-	#distance = main_div.next_sibling.children[3].text
+    #Future proofing for grabbing the distance
+    #distance = main_div.next_sibling.children[3].text
 
-	duration = main_div.at("p.dn-mob").text.split("M").last
-  solstice = main_div.next_sibling.children[5].text.split(": ").last
-  sunrise = main_div.next_sibling.children[6].children[1].text
-  sunset = main_div.next_sibling.children[7].children[1].text
+    duration = main_div.at("p.dn-mob").text.split("M").last
+    solstice = main_div.next_sibling.children[5].text.split(": ").last
+    sunrise = main_div.next_sibling.children[6].children[1].text
+    sunset = main_div.next_sibling.children[7].children[1].text
 
-  m.reply "In #{city}, #{country} the sun rises at #{sunrise} and sets at #{sunset}, a duration of #{duration}"
-  m.reply "The next solstice will be on #{solstice}"
+    m.reply "In #{city}, #{country} the sun rises at #{sunrise} and sets at #{sunset}, a duration of #{duration}"
+    m.reply "The next solstice will be on #{solstice}"
 
-	end
+  end
 
 end
--- test/unit/sun_info_test.rb
+++ test/unit/sun_info_test.rb
@@ -3,60 +3,60 @@ require 'sun_info'
 
 class SunInfoTest < MiniTest::Test
 
-	def test_sunrise_returns_am
-		bot = make_bot(SunInfo)
+  def test_sunrise_returns_am
+    bot = make_bot(SunInfo)
     message = make_message(bot, "!sun_info usa, chicago")
     replies = get_replies(message)
     output = get_replies(message).first
     assert output.include?("AM and")
-	end
+  end
 
-	def test_sunset_returns_pm
-		bot = make_bot(SunInfo)
+  def test_sunset_returns_pm
+    bot = make_bot(SunInfo)
     message = make_message(bot, "!sun_info usa, chicago")
     replies = get_replies(message)
     output = get_replies(message).first
     assert output.include?("PM,")
-	end
+  end
 
-	def test_duration_returns_hours
-		bot = make_bot(SunInfo)
+  def test_duration_returns_hours
+    bot = make_bot(SunInfo)
     message = make_message(bot, "!sun_info usa, chicago")
     replies = get_replies(message)
     output = get_replies(message).first
     assert output.include?("hours")
-	end
+  end
 
-	def test_duration_returns_minutes
-		bot = make_bot(SunInfo)
+  def test_duration_returns_minutes
+    bot = make_bot(SunInfo)
     message = make_message(bot, "!sun_info usa, chicago")
     replies = get_replies(message)
     output = get_replies(message).first
     assert output.include?("minutes")
-	end
+  end
 
-	def test_sun_info_returns_two_word_country
-		bot = make_bot(SunInfo)
+  def test_sun_info_returns_two_word_country
+    bot = make_bot(SunInfo)
     message = make_message(bot, "!sun_info saudi arabia, riyadh")
     replies = get_replies(message)
     output = get_replies(message).first
     assert output.include?("saudi arabia")
-	end
+  end
 
-	def test_sun_info_returns_two_word_city
-		bot = make_bot(SunInfo)
+  def test_sun_info_returns_two_word_city
+    bot = make_bot(SunInfo)
     message = make_message(bot, "!sun_info usa, eau claire")
     replies = get_replies(message)
     output = get_replies(message).first
     assert output.include?("eau claire")
-	end
+  end
 
-	def test_sun_info_returns_two_word_country_and_city
-		bot = make_bot(SunInfo)
+  def test_sun_info_returns_two_word_country_and_city
+    bot = make_bot(SunInfo)
     message = make_message(bot, "!sun_info saudi arabia, khamis mushait")
     replies = get_replies(message)
     output = get_replies(message).first
     assert output.include?("khamis mushait, saudi arabia")
-	end	
+  end	
 
 end
