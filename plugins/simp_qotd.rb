57b4dc81e7bf499f5913affa16df73a77b4c1d76
AlexanderHenry <alexanderhenry21@gmail.com>
2016-05-18 11:27:23 -0500

    Fixed spacing issues

--- plugins/simp_qotd.rb
+++ plugins/simp_qotd.rb
@@ -4,24 +4,24 @@ require 'cgi'
 
 class SimpQotd
 
-	$help_messages << "!simp_quote	: Provide Simpsons quote of the day with an image from @SimpsonsQOTD on Twitter"
+  $help_messages << "!simp_quote  : Provide Simpsons quote of the day with an image from @SimpsonsQOTD on Twitter"
   include Cinch::Plugin
 
   match /simp_quote/
 
   def execute(m)
 
-  	url = "https://twitter.com/SimpsonsQOTD"
+    url = "https://twitter.com/SimpsonsQOTD"
     ordered_list = Nokogiri::HTML(open(url)).at("ol#stream-items-id")
-    top_of_list = ordered_list.children[3]
+    top_of_list = ordered_list.children[1]
     recent_quote = top_of_list.at("p").text
     
     if top_of_list.at("div.PlayableMedia-player")
-    	video = top_of_list.at("div.PlayableMedia-player").attribute("style").to_s
-    	media_source = video.split("('").last.split("')").first
+      video = top_of_list.at("div.PlayableMedia-player").attribute("style").to_s
+      media_source = video.split("('").last.split("')").first
     else
-    	image = top_of_list.at("div.AdaptiveMedia-singlePhoto")
-    	media_source = image.at("img").attribute("src").to_s
+      image = top_of_list.at("div.AdaptiveMedia-singlePhoto")
+      media_source = image.at("img").attribute("src").to_s
     end
 
     m.reply "#{media_source}"
