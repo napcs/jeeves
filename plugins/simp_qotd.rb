250156adf4afac443d8b6a87678f68cc7a4a00e8
AlexanderHenry <alexanderhenry21@gmail.com>
2016-05-18 11:05:33 -0500

    Adding Simpsons Quote of the Day Plugin

---
+++ plugins/simp_qotd.rb
@@ -0,0 +1,32 @@
+require 'open-uri'
+require 'nokogiri'
+require 'cgi'
+
+class SimpQotd
+
+	$help_messages << "!simp_quote	: Provide Simpsons quote of the day with an image from @SimpsonsQOTD on Twitter"
+  include Cinch::Plugin
+
+  match /simp_quote/
+
+  def execute(m)
+
+  	url = "https://twitter.com/SimpsonsQOTD"
+    ordered_list = Nokogiri::HTML(open(url)).at("ol#stream-items-id")
+    top_of_list = ordered_list.children[3]
+    recent_quote = top_of_list.at("p").text
+    
+    if top_of_list.at("div.PlayableMedia-player")
+    	video = top_of_list.at("div.PlayableMedia-player").attribute("style").to_s
+    	media_source = video.split("('").last.split("')").first
+    else
+    	image = top_of_list.at("div.AdaptiveMedia-singlePhoto")
+    	media_source = image.at("img").attribute("src").to_s
+    end
+
+    m.reply "#{media_source}"
+    m.reply "#{recent_quote.split("pic.twitter").first}"
+
+  end
+
+end
