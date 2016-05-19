cc71e4a4d9fc0c6b05f6d21cc181d4038b602740
AlexanderHenry <alexanderhenry21@gmail.com>
2016-05-18 19:47:13 -0500

    Tests added

---
+++ test/unit/simp_qotd_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper.rb'
+require 'simp_qotd'
+
+class SimpQotdTest < MiniTest::Test
+
+  def test_link_is_returned
+    bot = make_bot(SimpQotd)
+    message = make_message(bot, "!simp_quote")
+    replies = get_replies(message)
+    output = get_replies(message).first
+    assert output.include?("https://pbs.twimg.com/")
+  end
+
+  def test_image_exists_in_link
+    bot = make_bot(SimpQotd)
+    message = make_message(bot, "!simp_quote")
+    replies = get_replies(message)
+    output = get_replies(message).first
+    assert output.include?(".jpg")
+  end
+end
